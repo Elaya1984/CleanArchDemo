44f3a4fce9e625ec9c17ccd0c4a7c2573184ced1
Elayaraja Muthuraj <50948988+ElayarajaMuthuraj@users.noreply.github.com>
2019-10-03 09:03:07 +0530

    Commit of register services

---
+++ CleanArch/CleanArch.Ioc/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchApp.Interfaces;
+using CleanArchApp.Services;
+using CleanArchInfra.Data.Repositories;
+using ClearArchDomain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Ioc
+{
+	public class DependencyContainer
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			//App Layer
+			services.AddScoped<ICourseService, CourseService>();
+
+			//Data layer
+			services.AddScoped<ICourseRepository, CourseRepository>();
+		}
+	}
+}
--- CleanArch/CleanArch.Mvc/Startup.cs
+++ CleanArch/CleanArch.Mvc/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using CleanArchInfra.Data.Context;
+using CleanArch.Ioc;
 
 namespace CleanArch.Mvc
 {
@@ -41,6 +42,7 @@ namespace CleanArch.Mvc
 
 			services.AddControllersWithViews();
 			services.AddRazorPages();
+			RegisterServices(services);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +74,11 @@ namespace CleanArch.Mvc
 					pattern: "{controller=Home}/{action=Index}/{id?}");
 				endpoints.MapRazorPages();
 			});
+		}
+
+		public static void RegisterServices(IServiceCollection services)
+		{
+			DependencyContainer.RegisterServices(services);
 		}
 	}
 }
