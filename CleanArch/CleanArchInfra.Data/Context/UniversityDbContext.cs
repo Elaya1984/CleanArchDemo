dba18e0272bf2c6cef4eaa17fb4a66888a2a803e
Elayaraja Muthuraj <50948988+ElayarajaMuthuraj@users.noreply.github.com>
2019-10-03 08:09:44 +0530

    Initial commit of infra db context

---
+++ CleanArch/CleanArchInfra.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ClearArchDomain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchInfra.Data.Context
+{
+	public class UniversityDbContext : DbContext
+	{
+		public UniversityDbContext(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Course> Courses { get; set; }
+	}
+}
