2c59de6fc37ee15d99ea31061e03f30db3ab0f33
Elayaraja Muthuraj <50948988+ElayarajaMuthuraj@users.noreply.github.com>
2019-10-03 09:36:07 +0530

    Commit of IUI implementation

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchApp.Interfaces;
+using CleanArchApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+		private ICourseService _courseService;
+		public CourseController(ICourseService courseService)
+		{
+			_courseService = courseService;
+		}
+        public IActionResult Index()
+        {
+			CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
